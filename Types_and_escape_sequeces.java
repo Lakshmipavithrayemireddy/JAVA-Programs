1609338fba45c8095ab5d3be66d542ef7a1d2727
Lakshmi Pavithra <168399713+Lakshmipavithrayemireddy@users.noreply.github.com>
2025-06-05 23:38:58 +0530

    Create Types_and_escape_sequeces.java

---
+++ Types_and_escape_sequeces.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main 
+{
+    public static void main(String[] args) 
+    {
+        byte weight=126;
+        short ages=20045;
+        int lakhs=2000056;
+        long billions=7889999l;
+        float pi=3.14f;
+        double number=9.6899d;
+        char letter='b';
+        String fruit="apple//orange//grape";//escaping sequences as \\ for \ and \" \" for " "   and \t for tab and \n for printing  in new line
+        String greet="good\t\"morning\"";
+        boolean isStudent=true;
+        System.out.println(weight);
+        System.out.println(ages);
+        System.out.println(lakhs);
+        System.out.println(billions);
+        System.out.println(pi);
+        System.out.println(number);
+        System.out.println(letter);
+        System.out.println(greet);
+        System.out.println(isStudent);
+        System.out.println(fruit);
+    }
+}
